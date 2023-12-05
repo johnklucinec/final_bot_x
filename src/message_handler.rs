7cd603a7820dd723ebbfb25c5714fb063f303e09
DaddyJuanito <jsklucinec@gmail.com>
2023-12-05 01:25:11 -0500

    added rancho tee hee

--- src/message_handler.rs
+++ src/message_handler.rs
@@ -1,14 +1,40 @@
 use serenity::{
+    all::{Http, ChannelId},
     client::Context,
     model::channel::Message
   };
 
 pub async fn message(ctx: Context, msg: Message) {
-    match msg.content.as_str() {
-        "!wintah" => {
+
+    // Listnen if the message is a command
+    if msg.content.starts_with('!') {
+
+        match msg.content.as_str() {
+            "!wintah" => {
+                send_message(&ctx.http, &msg.channel_id, "Yes, Wintah does indeed like men 🥵").await;
+            }
+    
+            "!burger" => {
+                send_message(&ctx.http, &msg.channel_id, "🍔").await;
+            }
+            
+            _ => {}
+        }
+
+    }
+
+    if msg.author.id != ctx.cache.current_user().id {
+        if msg.content.as_str().contains("rancho") {
+            send_message(&ctx.http, &msg.channel_id, "Who is Rancho?").await;
+        }
+     }
+
+
+    match msg.author.name.as_str() {
+        "racho" => {
             if let Err(why) = msg
                 .channel_id
-                .say(&ctx.http, "Yes, Wintah does indeed like men 🥵")
+                .say(&ctx.http, "Ok Rachel")
                 .await
             {
                 println!("Error sending message: {:?}", why);
@@ -17,16 +43,11 @@ pub async fn message(ctx: Context, msg: Message) {
         _ => {}
     }
 
-    match msg.author.name.as_str() {
-        "tyethetoster" => {
-            if let Err(why) = msg
-                .channel_id
-                .say(&ctx.http, "Nice balls bro!")
-                .await
-            {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-        _ => {}
+}
+
+// contruct commands that respond to messages
+async fn send_message(http: &Http, channel_id: &ChannelId, message: &str) {
+    if let Err(why) = channel_id.say(http, message).await {
+        println!("Error sending message: {:?}", why);
     }
 }
